698b0b1c7a2d2eef061132d970fa241c05343275
egagli <egagli@uw.edu>
2025-07-24 13:06:38 -0700

    add manual chunking step after masking

--- processing/scripts/process_single_tile.py
+++ processing/scripts/process_single_tile.py
@@ -197,7 +197,7 @@ def process_tile_github_actions(tile_row: int, tile_col: int, config):
             fail_on_error=True,
         )
 
-        s1_rtc_ds['vv'] = s1_rtc_ds['vv'].chunk({"latitude": 512, "longitude": 512, "time":300})# .chunk(config.chunks_s1_process) we don't do this with the serverless approach
+        # s1_rtc_ds['vv'] = s1_rtc_ds['vv'].chunk({"latitude": 512, "longitude": 512, "time":300})# .chunk(config.chunks_s1_process) we don't do this with the serverless approach
         # s1_rtc_ds['vv'] = s1_rtc_ds['vv'].astype(np.float16)
 
         # Check if lazily loaded
@@ -236,6 +236,9 @@ def process_tile_github_actions(tile_row: int, tile_col: int, config):
             spatiotemporal_snow_cover_mask_ds=spatiotemporal_snow_cover_mask_ds.chunk(chunks="auto"),#.chunk({"latitude": 128, "longitude": 128, "water_year": 1}),
             water_years=config.water_years,
         )
+        # probably very necessary to chunk here because s1_rtc_masked_ds chunking is 1 for time, NOT optimal
+        s1_rtc_masked_ds['vv'] = s1_rtc_masked_ds['vv'].chunk({"latitude": 512, "longitude": 512, "time":100})
+
         # Check if lazily loaded
         logging.info(f"Applied all masks to S1 RTC dataset "
                      f"(s1_rtc_masked_ds) - {dask_or_computed(s1_rtc_masked_ds)}")
