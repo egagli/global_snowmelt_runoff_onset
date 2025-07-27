2b8362974771915705e9302990624dcbf98a5ee0
egagli <egagli@uw.edu>
2025-07-27 14:45:56 -0700

    chunks again

--- processing/scripts/process_single_tile.py
+++ processing/scripts/process_single_tile.py
@@ -205,7 +205,7 @@ def process_tile_github_actions(tile_row: int, tile_col: int, config):
         else:
             resamp = "YS-APR"
 
-        s1_rtc_ds['vv'] = s1_rtc_ds['vv'].chunk({"latitude": 256, "longitude": 256, "time":30}) # xr.groupers.TimeResampler(resamp)
+        s1_rtc_ds['vv'] = s1_rtc_ds['vv'].chunk({"latitude": 512, "longitude": 512, "time":30}) # xr.groupers.TimeResampler(resamp)
         
         # Check if lazily loaded
         logging.info(f"Retrieved Sentinel-1 RTC dataset (s1_rtc_ds) - {dask_or_computed(s1_rtc_ds)}")
@@ -240,7 +240,7 @@ def process_tile_github_actions(tile_row: int, tile_col: int, config):
         s1_rtc_masked_ds = processing.apply_all_masks(
             s1_rtc_ds=s1_rtc_ds,
             gmba_clipped_gdf=gmba_clipped_gdf,
-            spatiotemporal_snow_cover_mask_ds=spatiotemporal_snow_cover_mask_ds.chunk({"latitude":256,"longitude":256,"water_year":1}),
+            spatiotemporal_snow_cover_mask_ds=spatiotemporal_snow_cover_mask_ds.chunk({"latitude":512,"longitude":512,"water_year":1}),
             water_years=config.water_years,
         )
 
