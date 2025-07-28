2c34702ca51c68a7f5a06690a1f0c9580978bd70
egagli <egagli@uw.edu>
2025-07-28 14:19:09 -0700

    new chunks

--- processing/scripts/process_single_tile.py
+++ processing/scripts/process_single_tile.py
@@ -193,7 +193,7 @@ def process_tile_github_actions(tile_row: int, tile_col: int, config):
             bands=config.bands,
             start_date=config.start_date,
             end_date=config.end_date,
-            chunks_read=config.chunks_s1_read,
+            chunks_read={'x':512,'y':512,'time':1},#config.chunks_s1_read,
             fail_on_error=False,
         )
 
@@ -205,7 +205,7 @@ def process_tile_github_actions(tile_row: int, tile_col: int, config):
         # else:
         #     resamp = "YS-APR"
 
-        s1_rtc_ds['vv'] = s1_rtc_ds['vv'].chunk({"latitude": 512, "longitude": 512, "time":5}) # xr.groupers.TimeResampler(resamp)
+        #s1_rtc_ds['vv'] = s1_rtc_ds['vv'].chunk({"latitude": 512, "longitude": 512, "time":5}) # xr.groupers.TimeResampler(resamp)
         
         # Check if lazily loaded
         logging.info(f"Retrieved Sentinel-1 RTC dataset (s1_rtc_ds) - {dask_or_computed(s1_rtc_ds)}")
