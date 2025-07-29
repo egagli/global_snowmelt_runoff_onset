64d405ccd531843c262df5f491a4643612236481
egagli <egagli@uw.edu>
2025-07-28 21:56:55 -0700

    same old

--- processing/scripts/process_single_tile.py
+++ processing/scripts/process_single_tile.py
@@ -197,7 +197,7 @@ def process_tile_github_actions(tile_row: int, tile_col: int, config):
             fail_on_error=False,
         )
 
-        s1_rtc_ds['vv'] = s1_rtc_ds['vv'].chunk({"latitude": 512, "longitude": 512, "time":1})# .chunk(config.chunks_s1_process) we don't do this with the serverless approach
+        s1_rtc_ds['vv'] = s1_rtc_ds['vv'].chunk({"latitude": 128, "longitude": 128, "time":1})# .chunk(config.chunks_s1_process) we don't do this with the serverless approach
         # s1_rtc_ds['vv'] = s1_rtc_ds['vv'].astype(np.float16)
 
         # if s1_rtc_ds.attrs['hemisphere'] == 'northern':
@@ -240,7 +240,7 @@ def process_tile_github_actions(tile_row: int, tile_col: int, config):
         s1_rtc_masked_ds = processing.apply_all_masks(
             s1_rtc_ds=s1_rtc_ds,
             gmba_clipped_gdf=gmba_clipped_gdf,
-            spatiotemporal_snow_cover_mask_ds=spatiotemporal_snow_cover_mask_ds.chunk({"latitude":512,"longitude":512,"water_year":1}),
+            spatiotemporal_snow_cover_mask_ds=spatiotemporal_snow_cover_mask_ds.chunk({"latitude":128,"longitude":128,"water_year":1}),
             water_years=config.water_years,
         )
 
