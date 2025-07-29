d8a5d4abb5307cb902b7134cbde37897513823a7
egagli <egagli@uw.edu>
2025-07-29 12:25:07 -0700

    whooops

--- processing/scripts/process_single_tile.py
+++ processing/scripts/process_single_tile.py
@@ -208,7 +208,7 @@ def process_tile_github_actions(tile_row: int, tile_col: int, config):
         #     s1_rtc_ds['vv'] = s1_rtc_ds['vv'].chunk({"latitude": 512, "longitude": 512, "time":30})# .chunk(config.chunks_s1_process) we don't do this with the serverless approach
         # else:
         #     thread_count = 4
-        s1_rtc_ds['vv'] = s1_rtc_ds['vv'].chunk(chunks="auto")
+        # s1_rtc_ds['vv'] = s1_rtc_ds['vv'].chunk({"latitude": 1024, "longitude": 1024, "time":1})
 
         thread_count = 16
         which_scheduler = 'threads'
@@ -248,7 +248,7 @@ def process_tile_github_actions(tile_row: int, tile_col: int, config):
         s1_rtc_masked_ds = processing.apply_all_masks(
             s1_rtc_ds=s1_rtc_ds,
             gmba_clipped_gdf=gmba_clipped_gdf,
-            spatiotemporal_snow_cover_mask_ds=spatiotemporal_snow_cover_mask_ds.chunk(chunks='auto'), #.chunk({"latitude":2048,"longitude":2048,"water_year":1}),
+            spatiotemporal_snow_cover_mask_ds=spatiotemporal_snow_cover_mask_ds.chunk({"latitude":2048,"longitude":2048,"water_year":1}),
             water_years=config.water_years,
         )
 
